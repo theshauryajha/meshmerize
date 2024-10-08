0b90ea7d1f611a699d86b6ec2a34be4337b20cd5
shaurya <shauryajha235@yahoo.com>
2024-10-08 12:18:10 +0530

    changed sensor test code for 8 sensors

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,35 +1,29 @@
-#define leftMostSensor 15
-#define leftCenterSensor 14
-#define centerSensor 13
-#define rightCenterSensor 12
-#define rightMostSensor 11
+ 0int sensorCount = 8;
+int sensorValues[sensorCount];
 
 void setup(){
   // put your setup code here, to run once:
-  pinMode(leftMostSensor, INPUT);
-  pinMode(leftCenterSensor, INPUT);
-  pinMode(centerSensor, INPUT);
-  pinMode(rightCenterSensor, INPUT);
-  pinMode(rightMostSensor, INPUT);
+  for (int i = 11; i <= 18; i++){
+    pinMode(i, INPUT);
+  }
 
   Serial.begin(9600);
 }
 
 void loop(){
-  // put your main code here, to run repeatedly:
-  int sensor1 = analogRead(leftMostSensor);
-  int sensor2 = analogRead(leftCenterSensor);
-  int sensor3 = analogRead(centerSensor);
-  int sensor4 = analogRead(rightCenterSensor);
-  int sensor5 = analogRead(rightMostSensor);
-
-  int sensorValues[5] ={sensor1, sensor2, sensor3, sensor4, sensor5};
-
-  for (int i = 0; i < 5; i++){
+  // put ypour main code here to run repeatedly:
+  read();
+  for (int i = 0; i < 8; i++){
     Serial.print(sensorValues[i]);
     Serial.print('\t');
   }
   Serial.println();
 
   delay(100);
+}
+
+void read(){
+  for (int i = 0; i < 8; i++){
+    sensorValues[i] = analogRead(i+11);
+  }
 }
