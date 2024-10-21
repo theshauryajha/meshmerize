fdd5453880e020aa4cf795ed35161a6f44c4371b
tkC0ding <tkjagani@gmail.com>
2024-10-21 22:55:46 +0530

    made changes to the sensor test code, need to now add pin numbers for the sensor array, led is connected to pin 10 of esp(basically the red led), and using only 12 sensorr out of 16.

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,39 +1,38 @@
-const int sensorCount = 8;
-int sensorValues[sensorCount];
+#include <QTRSensors.h>
 
-void setup(){
+QTRSensors qtr;
+
+const uint8_t SensorCount = 12;
+uint16_t sensorValues[SensorCount];
+int led_pin = 10;
+
+void setup() {
   // put your setup code here, to run once:
-  pinMode(A8,INPUT);
-  pinMode(A9,INPUT);
-  pinMode(A10,INPUT);
-  pinMode(A11,INPUT);
-  pinMode(A12,INPUT);
-  pinMode(A13,INPUT);
-  pinMode(A14,INPUT);
-  pinMode(A15,INPUT);
-
+  qtr.setTypeRC();
+  qtr.setSensorPins((const uint8_t[]){}, SensorCount);
+  pinMode(led_pin, OUTPUT);
+  calibrate_sensor();
   Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
   // put your main code here, to run repeatedly:
-  read();
-  for (int i = 0; i < 8; i++){
+
+  int position = qtr.readLineWhite(sensorValues);
+
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
     Serial.print(sensorValues[i]);
     Serial.print('\t');
   }
-  Serial.println();
-
-  delay(100);
+  Serial.println(position);
 }
 
-void read(){
-  sensorValues[0] = analogRead(A8);
-  sensorValues[1] = analogRead(A9);
-  sensorValues[2] = analogRead(A10);
-  sensorValues[3] = analogRead(A11);
-  sensorValues[4] = analogRead(A12);
-  sensorValues[5] = analogRead(A13);
-  sensorValues[6] = analogRead(A14);
-  sensorValues[7] = analogRead(A15);  
+void calibrate_sensor(){
+  digitalWrite(led_pin, HIGH);
+  for (uint16_t i = 0; i < 400; i++)
+  {
+    qtr.calibrate();
+  }
+  digitalWrite(led_pin, LOW);
 }
