16cef4bbfde4b67d45c16169e366ddc6ba974aed
shaurya <shauryajha235@yahoo.com>
2024-10-22 11:20:17 +0530

    good sensor values for bang bang

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,38 +1,30 @@
-#include <QTRSensors.h>
+#define sensorCount 8
+const int sensorPins[sensorCount] ={15, 2, 4, 16, 5, 13, 12, 14};
+int sensorValues[sensorCount];
 
-QTRSensors qtr;
-
-const uint8_t SensorCount = 12;
-uint16_t sensorValues[SensorCount];
-int led_pin = 10;
-
-void setup() {
+void setup(){
   // put your setup code here, to run once:
-  qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]){}, SensorCount);
-  pinMode(led_pin, OUTPUT);
-  calibrate_sensor();
+  for (int i = 0; i < sensorCount; i++){
+    pinMode(sensorPins[i], INPUT);
+  }
+
   Serial.begin(9600);
 }
 
-void loop() {
+void loop(){
   // put your main code here, to run repeatedly:
+  read();
 
-  int position = qtr.readLineWhite(sensorValues);
-
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
+  for (int i = 0; i < sensorCount; i++){
     Serial.print(sensorValues[i]);
     Serial.print('\t');
   }
-  Serial.println(position);
+  Serial.println();
+  delay(100);
 }
 
-void calibrate_sensor(){
-  digitalWrite(led_pin, HIGH);
-  for (uint16_t i = 0; i < 400; i++)
-  {
-    qtr.calibrate();
+void read(){
+  for (int i = 0; i < sensorCount; i++){
+    sensorValues[i] = digitalRead(sensorPins[i]);
   }
-  digitalWrite(led_pin, LOW);
 }
