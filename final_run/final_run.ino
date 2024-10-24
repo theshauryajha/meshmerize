d503ac88343e724b8f4adba0e8fddac476c75f76
shaurya <shauryajha235@yahoo.com>
2024-10-24 07:46:16 +0530

    final run code

---
+++ final_run/final_run.ino
@@ -0,0 +1,414 @@
+#include <BluetoothSerial.h>
+#include <QTRSensors.h>
+
+// in1  - 19
+// in 2 - 23
+// in 3 - 21
+// in 4 - 2
+#define RH 21  // Right motor control
+#define LH 19  // Left motor control
+#define RL 2   // Reverse Right motor control
+#define LL 23  // Reverse Left motor control
+
+#define LED_PIN 12
+
+// Pololu sensor setup
+QTRSensors qtrrc;
+int basespeed = 100;         // Base motor speed
+int maxSpeed = 200;         // Maximum motor speed
+uint16_t sensorValues[12];  // Array to store sensor values
+
+// PID constants
+float Kp = 0.035;  // Proportional gain
+float Ki = 0.0;    // Integral gain
+float Kd = 0.044;   // Derivative gain
+int integral = 0;
+int lastError = 0;
+char myData[30] = { 0 }, s1[10], s2[10], s3[10], s4[10];
+
+int leftMultiplier[] = { 0, 1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0 };
+int straightMultiplier[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+int rightMultiplier[] = { 0, 0, 0, 0, 0, 5, 6, 7, 8, 9, 10, 11 };
+
+
+int count = 0;
+char path[] = "SRLRS";
+
+void setup() {
+  // Set motor pins as outputs
+  pinMode(RH, OUTPUT);
+  pinMode(RL, OUTPUT);
+  pinMode(LL, OUTPUT);
+  pinMode(LH, OUTPUT);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  // Initialize sensor array
+  qtrrc.setTypeRC();
+  qtrrc.setSensorPins((const uint8_t[]){ 13, 15, 14, 4, 27, 16, 26, 17, 25, 5, 33, 18 }, 12);
+  Serial.begin(9600);
+  // Serial.begin("PID_Tuner"); // Bluetooth device name
+
+  Serial.println("Bluetooth Device is Ready to Pair");
+  // Serial.println("Bluetooth Device is Ready to Pair");
+
+  // Calibrate sensors
+  for (int i = 0; i < 250; i++) {
+    qtrrc.calibrate();
+    delay(20);
+    Serial.println("Calibrating...");
+    //Serial.println("Calibrating...");
+  }
+}
+
+// int counter = 0;
+void loop() {
+  Serial.println("");
+  // counter += 1;
+  // Check for incoming Bluetooth Serial data
+
+  if (Serial.available()) {
+    byte m = Serial.readBytesUntil('\n', myData, 20);
+    myData[m] = '\0';  //null-byte
+    if (sscanf(myData, "%[^','],%[^','],%[^','],%[^','],%s", s1, s2, s4, s3) == 4) {
+      Kp = atof(s1);
+      Kd = atof(s2);
+      maxSpeed = atof(s3);
+      basespeed = atof(s4);
+      Serial.println("values updated");
+
+    } else Serial.println("error in input!");
+  }
+
+  qtrrc.read(sensorValues);
+  for (int i = 0; i < 12; i++){
+    map(sensorValues[i], 0, 2500, 0, 1000);
+  }
+
+  int position;
+  if (intersection() == 1) {
+    if (path[count] == 'L')
+      left();
+    else if (path[count] == 'R')
+      right();
+    else
+      straight();
+    count++;
+  }
+  else if (intersection() == -1)
+    stop();
+
+  position = qtrrc.readLineWhite(sensorValues);
+
+  
+  /*for (int i = 0; i < 12; i++){
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
+  }*/
+
+
+  // Compute error (desired position is 5500 for 12-sensor array)
+  int error = position - 5500;
+  integral += error;                                        // Calculate integral component
+  int derivative = error - lastError;                       // Calculate derivative component
+  int turn = Kp * error + Ki * integral + Kd * derivative;  // Calculate PID value
+  lastError = error;                                        // Store current error as lastError for next iteration
+
+  // Adjust motor speeds based on PID value
+  int leftMotorSpeed = basespeed + turn;
+  int rightMotorSpeed = basespeed - turn;
+
+  // Ensure motor speeds are within bounds
+  if (leftMotorSpeed > maxSpeed) leftMotorSpeed = maxSpeed;
+  if (rightMotorSpeed > maxSpeed) rightMotorSpeed = maxSpeed;
+  // if (leftMotorSpeed < 0 && leftMotorSpeed > -70) leftMotorSpeed = 0;
+  // if (rightMotorSpeed < 0  && rightMotorSpeed > -70) rightMotorSpeed = 0;
+  if (leftMotorSpeed < 0) leftMotorSpeed = -basespeed;
+  if (rightMotorSpeed < 0) rightMotorSpeed = -basespeed;
+
+  // Motor control logic
+  if (leftMotorSpeed < 0) {
+    analogWrite(LL, leftMotorSpeed);  // Reverse left motor
+    analogWrite(LH, 0);
+  } else {
+    analogWrite(LH, leftMotorSpeed);
+    analogWrite(LL, 0);
+  }
+
+  if (rightMotorSpeed < 0) {
+    analogWrite(RL, rightMotorSpeed);  // Reverse right motor
+    analogWrite(RH, 0);
+  } else {
+    analogWrite(RH, rightMotorSpeed);
+    analogWrite(RL, 0);
+  }
+
+
+  // if (counter % 10 == 0) {
+  for (int i = 0; i < 12; i++) {
+    Serial.print(sensorValues[i] > 500 ? 1 : 0);
+    // Serial.print(" ");
+  }
+  Serial.print(" ");
+  // }
+
+  // Output debugging information
+  // Serial.print("Error: ");
+  // Serial.print(error);
+  // Serial.print("\t");
+  // Serial.print("Left Speed: ");
+  // Serial.print(leftMotorSpeed);
+  // Serial.print("\t");
+  // Serial.print("Right Speed: ");
+  // Serial.print(rightMotorSpeed);
+  // Serial.print("\t");
+  // Serial.print(Kp);
+  // Serial.print("\t");
+  // Serial.print(Kd);
+  // Serial.print("\t");
+  // Serial.println(basespeed);
+
+  // Send debugging information to Bluetooth
+  //  if (counter % 10 == 0) {
+  // Serial.print("E ");
+  // Serial.print(error);
+  // // Serial.print(" ");
+  // Serial.print(" L ");
+  // Serial.print(leftMotorSpeed);
+  // // Serial.print(" ");
+  // Serial.print(" R ");
+  // Serial.print(rightMotorSpeed);
+  // Serial.print("  Kp ");
+  // Serial.print(Kp);
+  // Serial.print("  Kd");
+  // Serial.println(Kd);
+  //  }
+  // for (int i = 0; i < )
+
+
+
+  //   if (Serial.available()) {
+  //     String command = Serial.readStringUntil('\n');
+  //     handleCommand(command);
+  //   }// Add delay to avoid overloading the motors
+  // }
+}
+
+
+int weightedRead(int multipliers[]) {
+  int multiplierSum = 0, totalSum = 0;
+  for (int i = 0; i < 12; i++) {
+    multiplierSum += multipliers[i] * sensorValues[i];
+    totalSum += i * 1000;
+  }
+
+  int position = 1000 * multiplierSum / totalSum;
+  return position;
+}
+
+void left(){
+  int position = weightedRead(leftMultiplier);
+  // Compute error (desired position is 5500 for 12-sensor array)
+  int error = position - 5500;
+  integral += error;                                        // Calculate integral component
+  int derivative = error - lastError;                       // Calculate derivative component
+  int turn = Kp * error + Ki * integral + Kd * derivative;  // Calculate PID value
+  lastError = error;                                        // Store current error as lastError for next iteration
+
+  // Adjust motor speeds based on PID value
+  int leftMotorSpeed = basespeed + turn;
+  int rightMotorSpeed = basespeed - turn;
+
+  // Ensure motor speeds are within bounds
+  if (leftMotorSpeed > maxSpeed) leftMotorSpeed = maxSpeed;
+  if (rightMotorSpeed > maxSpeed) rightMotorSpeed = maxSpeed;
+  // if (leftMotorSpeed < 0 && leftMotorSpeed > -70) leftMotorSpeed = 0;
+  // if (rightMotorSpeed < 0  && rightMotorSpeed > -70) rightMotorSpeed = 0;
+  if (leftMotorSpeed < 0) leftMotorSpeed = -basespeed;
+  if (rightMotorSpeed < 0) rightMotorSpeed = -basespeed;
+
+  // Motor control logic
+  if (leftMotorSpeed < 0) {
+    analogWrite(LL, leftMotorSpeed);  // Reverse left motor
+    analogWrite(LH, 0);
+  } else {
+    analogWrite(LH, leftMotorSpeed);
+    analogWrite(LL, 0);
+  }
+
+  if (rightMotorSpeed < 0) {
+    analogWrite(RL, rightMotorSpeed);  // Reverse right motor
+    analogWrite(RH, 0);
+  } else {
+    analogWrite(RH, rightMotorSpeed);
+    analogWrite(RL, 0);
+  }
+
+  delay(100);
+}
+
+void right(){
+  int position = weightedRead(rightMultiplier);
+  // Compute error (desired position is 5500 for 12-sensor array)
+  int error = position - 5500;
+  integral += error;                                        // Calculate integral component
+  int derivative = error - lastError;                       // Calculate derivative component
+  int turn = Kp * error + Ki * integral + Kd * derivative;  // Calculate PID value
+  lastError = error;                                        // Store current error as lastError for next iteration
+
+  // Adjust motor speeds based on PID value
+  int leftMotorSpeed = basespeed + turn;
+  int rightMotorSpeed = basespeed - turn;
+
+  // Ensure motor speeds are within bounds
+  if (leftMotorSpeed > maxSpeed) leftMotorSpeed = maxSpeed;
+  if (rightMotorSpeed > maxSpeed) rightMotorSpeed = maxSpeed;
+  // if (leftMotorSpeed < 0 && leftMotorSpeed > -70) leftMotorSpeed = 0;
+  // if (rightMotorSpeed < 0  && rightMotorSpeed > -70) rightMotorSpeed = 0;
+  if (leftMotorSpeed < 0) leftMotorSpeed = -basespeed;
+  if (rightMotorSpeed < 0) rightMotorSpeed = -basespeed;
+
+  // Motor control logic
+  if (leftMotorSpeed < 0) {
+    analogWrite(LL, leftMotorSpeed);  // Reverse left motor
+    analogWrite(LH, 0);
+  } else {
+    analogWrite(LH, leftMotorSpeed);
+    analogWrite(LL, 0);
+  }
+
+  if (rightMotorSpeed < 0) {
+    analogWrite(RL, rightMotorSpeed);  // Reverse right motor
+    analogWrite(RH, 0);
+  } else {
+    analogWrite(RH, rightMotorSpeed);
+    analogWrite(RL, 0);
+  }
+
+  delay(100);
+}
+
+void straight(){
+  int position = weightedRead(straightMultiplier);
+  // Compute error (desired position is 5500 for 12-sensor array)
+  int error = position - 5500;
+  integral += error;                                        // Calculate integral component
+  int derivative = error - lastError;                       // Calculate derivative component
+  int turn = Kp * error + Ki * integral + Kd * derivative;  // Calculate PID value
+  lastError = error;                                        // Store current error as lastError for next iteration
+
+  // Adjust motor speeds based on PID value
+  int leftMotorSpeed = basespeed + turn;
+  int rightMotorSpeed = basespeed - turn;
+
+  // Ensure motor speeds are within bounds
+  if (leftMotorSpeed > maxSpeed) leftMotorSpeed = maxSpeed;
+  if (rightMotorSpeed > maxSpeed) rightMotorSpeed = maxSpeed;
+  // if (leftMotorSpeed < 0 && leftMotorSpeed > -70) leftMotorSpeed = 0;
+  // if (rightMotorSpeed < 0  && rightMotorSpeed > -70) rightMotorSpeed = 0;
+  if (leftMotorSpeed < 0) leftMotorSpeed = -basespeed;
+  if (rightMotorSpeed < 0) rightMotorSpeed = -basespeed;
+
+  // Motor control logic
+  if (leftMotorSpeed < 0) {
+    analogWrite(LL, leftMotorSpeed);  // Reverse left motor
+    analogWrite(LH, 0);
+  } else {
+    analogWrite(LH, leftMotorSpeed);
+    analogWrite(LL, 0);
+  }
+
+  if (rightMotorSpeed < 0) {
+    analogWrite(RL, rightMotorSpeed);  // Reverse right motor
+    analogWrite(RH, 0);
+  } else {
+    analogWrite(RH, rightMotorSpeed);
+    analogWrite(RL, 0);
+  }
+
+  delay(100);
+}
+
+void stop(){
+  analogWrite(LH, 0);
+  analogWrite(LL, 0);
+  analogWrite(RH, 0);
+  analogWrite(RL, 0);
+
+  digitalWrite(LED_PIN, HIGH);
+}
+
+int intersection(){
+  bool left = sensorValues[0] > 500;
+  bool straight = sensorValues[5] > 500 && sensorValues[6] > 500;
+  bool right = sensorValues[11] > 500;
+
+  Serial.print("L");
+  Serial.print(left);
+  Serial.print("R");
+  Serial.print(right);
+  Serial.print("S");
+  Serial.print(straight);
+
+  if (left && right){
+    stop();
+    delay(1000);
+    extraInch();
+    stop();
+    delay(1000);
+    if (left && right)
+      return -1; // maze end
+    else{
+      extraInchBack();
+      stop();
+      delay(1000);
+      return 1; // LR T-junc
+    }
+  }
+  else if (left && straight){
+    stop();
+    delay(1000);
+    extraInch();
+    stop();
+    delay(1000);
+    if (straight) return 1; // LS T-junc
+    else{
+      extraInchBack();
+      stop();
+      delay(1000);
+      return 0; // no intersection
+    }
+  }
+  else if (straight && right){
+    stop();
+    delay(1000);
+    extraInch();
+    stop();
+    delay(1000);
+    if (straight) return 1; // RS T-junc
+    else{
+      extraInchBack();
+      stop();
+      delay(1000);
+      return 0; // no intersection
+    }
+  }
+  return 0; // no intersection
+}
+
+void extraInch(){
+  analogWrite(LH, basespeed);
+  analogWrite(LL, 0);
+  analogWrite(RH, basespeed);
+  analogWrite(RL, 0);
+  delay(10);
+}
+
+void extraInchBack(){
+  analogWrite(LH, 0);
+  analogWrite(LL, basespeed);
+  analogWrite(RH, 0);
+  analogWrite(RL, basespeed);
+  delay(10);
+}
--- pd_lsrb/pd_lsrb.ino
+++ pd_lsrb/pd_lsrb.ino
@@ -1,247 +1,163 @@
+// imports
+#include <BluetoothSerial.h>
 #include <QTRSensors.h>
-#include <vector>
 
-using namespace std;
+// ESP - Motor Pin Connections
+#define LH 19 // IN1
+#define LL 23 // IN2
+#define RH 21 // IN3
+#define RL 2 // IN4
 
-// Motor and encoder pins
-//left motor pins
-#define LEFT_MOTOR_PIN1 2
-#define LEFT_MOTOR_PIN2 3
-#define LEFT_MOTOR_PWM 1 //dummy pin value
-#define LEFT_ENCODER_PIN 18
 
-//right motor pins
-#define RIGHT_MOTOR_PIN1 4
-#define RIGHT_MOTOR_PIN2 5
-#define RIGHT_MOTOR_PWM 10 //dummy pin value
-#define RIGHT_ENCODER_PIN 19
+// Pololu sensor setup
+#define sensorCount 12
+QTRSensors qtrrc;
+int basespeed = 75; // Base motor speed
+int maxSpeed = 100; // Maximum motor speed
+uint16_t sensorValues[sensorCount]; // Array to store sensor values
 
-// PD constants
-#define KP 0.1
-#define KD 0.5
-
-//LSRB dry run vector
-vector<char> path;
-
-// Sensor array
-QTRSensors qtr;
-const uint8_t SensorCount = 8;
-uint16_t sensorValues[SensorCount];
-
-// Motor control variables
-int leftSpeed = 0;
-int rightSpeed = 0;
-const int baseSpeed = 150;
-
-// Error variable
+// PID constants
+float Kp = 0.005;  // Proportional gain
+float Ki = 0.0;  // Integral gain
+float Kd = 0.05;  // Derivative gain
+int integral = 0;
 int lastError = 0;
+char myData[30] = { 0 } , s1[10], s2[10],s3[10],s4[10];
 
-// Maze solving variables
-char path[100];
-int pathLength = 0;
+void setup() {
+  // Set motor pins as outputs
+  pinMode(RH, OUTPUT);
+  pinMode(RL, OUTPUT);
+  pinMode(LL, OUTPUT);
+  pinMode(LH, OUTPUT);
 
-// Threshold to detect white line
-const int threshold = 900;
-
-void setup(){
-  // Pin configurations
-  pinMode(LEFT_MOTOR_PIN1, OUTPUT);
-  pinMode(LEFT_MOTOR_PIN2, OUTPUT);
-  pinMode(LEFT_MOTOR_PWM, OUTPUT);
-  pinMode(LEFT_ENCODER_PIN, INPUT_PULLUP);
-
-  pinMode(RIGHT_MOTOR_PIN1, OUTPUT);
-  pinMode(RIGHT_MOTOR_PIN2, OUTPUT);
-  pinMode(RIGHT_MOTOR_PWM, OUTPUT);
-  pinMode(RIGHT_ENCODER_PIN, INPUT_PULLUP);
-
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  // Configure sensor array
-  qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]){30, 31, 32, 33, 34, 35, 36, 37}, SensorCount);
-  
-  calibrateSensors();
-
+  // Initialize sensor array
+  qtrrc.setTypeRC();
+  qtrrc.setSensorPins((const uint8_t[]){13, 15, 14, 4, 27, 16, 26, 17, 25, 5, 33, 18}, sensorCount);
   Serial.begin(9600);
-}
+ // Serial.begin("PID_Tuner"); // Bluetooth device name
 
-void loop(){
-  // Read sensor values
-  uint16_t position = qtr.readLineWhite(sensorValues);
+  Serial.println("Bluetooth Device is Ready to Pair");
+ // Serial.println("Bluetooth Device is Ready to Pair");
 
-  // PD calculation
-  int error = position - 3500;
-  int derivative = error - lastError;
-  int motorSpeed = KP * error + KD * derivative;
-
-  leftSpeed = baseSpeed + motorSpeed;
-  rightSpeed = baseSpeed - motorSpeed;
-
-  leftSpeed = constrain(leftSpeed, 50, 255);
-  rightSpeed = constrain(rightSpeed, 50, 255);
-
-  // Check for intersection
-  int intersectionType = checkIntersection();
-  handleIntersection(intersectionType);
-
-  // Always use PID-controlled motor speeds
-  driveMotors(leftSpeed, rightSpeed);
-
-  lastError = error;
-}
-
-void calibrateSensors(){
-  digitalWrite(LED_BUILIIN, HIGH);
-  for (uint16_t i = 0; i < 400; i++){
-    qtr.calibrate();
-  }
-  digitalWrite(LED_BUILTIN, LOW); //calibration takes 10s, led will be on for 10s
-}
-
-void driveMotors(int left, int right){
-  driveMotorLeft(left);
-  driveMotorRight(right);
-}
-
-void driveMotorRight(int right){
-  digitalWrite(RIGHT_MOTOR_PIN1, HIGH);
-  digitalWrite(RIGHT_MOTOR_PIN2, LOW);
-
-  analogWrite(RIGHT_MOTOR_PWM, right);
-}
-
-void driveMotorLeft(int left){
-  digitalWrite(LEFT_MOTOR_PIN1, HIGH);
-  digitalWrite(LEFT_MOTOR_PIN2, LOW);
-
-  analogWrite(LEFT_MOTOR_PWM, left);
-}
-
-
-void turnLeft(){
-  while (sensorValues[0] > threshold || sensorValues[1] > threshold){
-    qtr.readLineWhite(sensorValues);
-    int error = sensorValues[0] - sensorValues[7];
-    int derivative = error - lastError;
-    int motorSpeed = KP * error + KD * derivative;
-    
-    leftSpeed = -baseSpeed + motorSpeed;
-    rightSpeed = baseSpeed + motorSpeed;
-    
-    leftSpeed = constrain(leftSpeed, -255, 255);
-    rightSpeed = constrain(rightSpeed, -255, 255);
-    
-    driveMotors(leftSpeed, rightSpeed);
-    
-    lastError = error;
+  // Calibrate sensors
+  for (int i = 0; i < 250; i++) {
+    qtrrc.calibrate();
+    delay(20);
+    Serial.println("Calibrating...");
   }
 }
 
-void turnRight(){
-  while (sensorValues[7] > threshold || sensorValues[6] > threshold){
-    qtr.readLineWhite(sensorValues);
-    int error = sensorValues[7] - sensorValues[0];
-    int derivative = error - lastError;
-    int motorSpeed = KP * error + KD * derivative;
-    
-    leftSpeed = baseSpeed + motorSpeed;
-    rightSpeed = -baseSpeed + motorSpeed;
-    
-    leftSpeed = constrain(leftSpeed, -255, 255);
-    rightSpeed = constrain(rightSpeed, -255, 255);
-    
-    driveMotors(leftSpeed, rightSpeed);
-    
-    lastError = error;
-  }
-}
+// int counter = 0;
+void loop() {
+  Serial.println(""); 
+  // counter += 1;
 
-void turnAround(){
-  // First, turn right until we lose the line
-  while (sensorValues[3] > threshold || sensorValues[4] > threshold){
-    qtr.readLineWhite(sensorValues);
-    driveMotors(baseSpeed, -baseSpeed);
-  }
-  // Then, turn right until we find the line again
-  while (sensorValues[3] < threshold && sensorValues[4] < threshold){
-    qtr.readLineWhite(sensorValues);
-    driveMotors(baseSpeed, -baseSpeed);
-  }
-}
-
-int checkIntersection(){
-  uint16_t right_sensor = sensorValues[7];
-  uint16_t left_sensor = sensorValues[0];
-  uint16_t sensor_4 = sensorValues[3];
-  uint16_t sensor_5 = sensorValues[4];
-
-  bool LINE = sensor_4 >= threshold && sensor_5 >= threshold;
-  bool RIGHT = LINE && right_sensor >= threshold;
-  bool LEFT = LINE && left_sensor >= threshold;
-
-  if(LINE){ //check is there is a line detected
-    if(RIGHT && LEFT){ //if line is there then check if there are right and left turns at the same place
-      extra_inch(); //if yes then run an extra inch and stop
-
-      bool if_end = check_end(LINE, RIGHT, LEFT); //check if we have reached the end of the maze or not
-
-      if(if_end){
-        return 0;
-      }
-
-      if(LINE){ //now check if there is a line or not
-        path.push_back('L')
-        return 4; //if line is there means it is a 4 way cross section thus return 4
-      }else{
-        path.push_back('L');
-        return 3; //if line is not there then it is a 3 way T junction with both right and left turn thus return 3 
-      }
+  // read BT data and check it
+  if (Serial.available()){
+    byte m = Serial.readBytesUntil('\n', myData, 20);
+    myData[m] = '\0';  //null-byte
+    if (sscanf(myData, "%[^','],%[^','],%[^','],%[^','],%s", s1, s2,s4,s3) == 4) {
+      Kp = atof(s1);
+      Kd = atof(s2);
+      maxSpeed = atof(s3);
+      basespeed=atof(s4);
+      Serial.println("values updated");
     }
-    else if(RIGHT){ //if there is only right turn
-      extra_inch(); //run an extra inch and stop
-
-      if(LINE){ //check for a line
-        path.push_back('S');
-        return 2; //if line is there means there is right turn and straight, return 2
-      }else{
-        path.push_back('R');
-        return 1; //if line is not there means it is a hard right, return 1
-      }
-    }
-    else if(LEFT){ //if there is only left turn
-      extra_inch(); //run an extra inch and stop
-
-      if(LINE){ //check for a line
-        path.push_back('L');
-        return 5; //if line is there then it is a 3 way junction with straight and left turn, return 5
-      }else{
-        path.push_back('L');
-        return 6; //if line is not there then it is a hard left, return 6
-      }
-    }
-    return 7; //if none of the cases are true means there is only a line, return 7
-  }else{
-    path.push_back('B');
-    return 8; //if there is no line then return 8;
+    else
+      Serial.println("error in input!");
   }
-}
-
-void handleIntersection(int intersectionType){
   
-}
 
-//stop the bot
-void breaks(){
-  digitalWrite(RIGHT_MOTOR_PIN1, LOW);
-  digitalWrite(RIGHT_MOTOR_PIN2, LOW);
+  // Read sensor values
+  int position = qtrrc.readLineWhite(sensorValues);  // Get position based on weighted sum of sensor readings
 
-  digitalWrite(LEFT_MOTOR_PIN1, LOW);
-  digitalWrite(LEFT_MOTOR_PIN2, LOW);
-}
 
-//the bot moves an extra inch and stops
-void extra_inch(){
-  delay(57);
-  breaks();
+  // Compute error (desired position is 5500 for 12-sensor array)
+  int error = position - 5500;
+  integral += error;  // Calculate integral component
+  int derivative = error - lastError;  // Calculate derivative component
+  int turn = Kp * error + Ki * integral + Kd * derivative;  // Calculate PID value
+  lastError = error;  // Store current error as lastError for next iteration
+
+  // Adjust motor speeds based on PID value
+  int leftMotorSpeed = basespeed + turn;
+  int rightMotorSpeed = basespeed - turn;
+
+  // Ensure motor speeds are within bounds
+  if (leftMotorSpeed > maxSpeed) leftMotorSpeed = maxSpeed;
+  if (rightMotorSpeed > maxSpeed) rightMotorSpeed = maxSpeed;
+  // if (leftMotorSpeed < 0 && leftMotorSpeed > -70) leftMotorSpeed = 0;
+  // if (rightMotorSpeed < 0  && rightMotorSpeed > -70) rightMotorSpeed = 0;
+  if (leftMotorSpeed < 0) leftMotorSpeed = -basespeed;
+  if (rightMotorSpeed < 0) rightMotorSpeed = -basespeed;  
+
+  // Motor control logic
+  if (leftMotorSpeed < 0) {
+    analogWrite(LL, leftMotorSpeed);  // Reverse left motor
+    analogWrite(LH, 0);
+  } else {
+    analogWrite(LH, leftMotorSpeed);
+    analogWrite(LL, 0);
+  }
+
+  if (rightMotorSpeed < 0) {
+    analogWrite(RL, rightMotorSpeed);  // Reverse right motor
+    analogWrite(RH, 0);
+  } else {
+    analogWrite(RH, rightMotorSpeed);
+    analogWrite(RL, 0);
+  }
+
+
+// if (counter % 10 == 0) {
+  for (int i = 0; i < 12; i++) {
+    Serial.print(sensorValues[i] > 500 ? 1 : 0);
+    // Serial.print(" ");
+  }
+  Serial.print(" ");
+// }
+
+  // Output debugging information
+  // Serial.print("Error: ");
+  // Serial.print(error);
+  // Serial.print("\t");
+  // Serial.print("Left Speed: ");
+  // Serial.print(leftMotorSpeed);
+  // Serial.print("\t");
+  // Serial.print("Right Speed: ");
+  // Serial.print(rightMotorSpeed);
+  // Serial.print("\t");
+  // Serial.print(Kp);
+  // Serial.print("\t");
+  // Serial.print(Kd);
+  // Serial.print("\t");
+  // Serial.println(basespeed);
+
+  // Send debugging information to Bluetooth
+//  if (counter % 10 == 0) {
+  Serial.print("E ");
+  Serial.print(error);
+  // Serial.print(" ");
+  Serial.print(" L ");
+  Serial.print(leftMotorSpeed);
+  // Serial.print(" ");
+  Serial.print(" R ");
+  Serial.print(rightMotorSpeed);
+  Serial.print("  Kp ");
+  Serial.print(Kp);
+  Serial.print("  Kd");
+  Serial.println(Kd);
+//  }
+  // for (int i = 0; i < )
+  
+  
+  
+//   if (Serial.available()) {
+//     String command = Serial.readStringUntil('\n');
+//     handleCommand(command);
+//   }// Add delay to avoid overloading the motors
+// }
+
+
 }
